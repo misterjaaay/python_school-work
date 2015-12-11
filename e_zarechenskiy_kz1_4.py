43c07898632e92b4f1f643e3bc6e2821d331f3fd
jay <misterjaaay@gmail.com>
2015-12-11 18:53:34 +0200

    works as required

--- e_zarechenskiy_kz1_4.py
+++ e_zarechenskiy_kz1_4.py
@@ -2,12 +2,12 @@
 # coding: utf-8
 
 var_string = 'wowhatamanwowowowpalehche'
-ind = 1
+index = 1
 count = 0
 template = 'wow'
-while ind != -1:
-    ind = var_string.find(template)
-    if ind >= 0:
+while index != -1:
+    index = var_string.find(template)
+    if index >= 0:
         count += 1
-    var_string = var_string[ind+1:]
+    var_string = var_string[index+1:]
 print count
