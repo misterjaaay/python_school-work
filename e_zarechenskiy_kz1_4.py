91beb146ce80c5372dbe916c9df53d7497a6b4b9
jay <misterjaaay@gmail.com>
2015-12-11 18:52:23 +0200

    works as required

--- e_zarechenskiy_kz1_4.py
+++ e_zarechenskiy_kz1_4.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python
 # coding: utf-8
-#var_string = "wowhatamanwowowpalehche"   count = 3
 
-var_string = 'wowhatamanwowowpalehche'
-print(var_string.count('wow'))
+var_string = 'wowhatamanwowowowpalehche'
+ind = 1
+count = 0
+template = 'wow'
+while ind != -1:
+    ind = var_string.find(template)
+    if ind >= 0:
+        count += 1
+    var_string = var_string[ind+1:]
+print count
