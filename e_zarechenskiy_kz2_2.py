f77e205831ce2fa4cc8ad75275d20f299d13260f
jay <misterjaaay@gmail.com>
2015-12-13 16:16:08 +0200

    sorted string

---
+++ e_zarechenskiy_kz2_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# coding: utf-8
+
+string = '1486371'
+even = ""
+odd = ""
+
+for i in string:
+	if int(i) % 2 == 0:
+		even += i
+	else:
+		odd += i
+		
+odd = sorted(odd)
+even = sorted(even)
+change_string = odd+even[::-1] 
+change_string = "".join(odd+even[::-1] )
+print change_string
